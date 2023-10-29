3695a71ae7bd90836b23a07715c104e1d8fabe70
Kamal Bijaraniyan <52608133+mfluckyxd@users.noreply.github.com>
2023-10-29 18:43:14 +0530

    Update currencyModel.js

--- models/currencyModel.js
+++ models/currencyModel.js
@@ -4,6 +4,15 @@ const currencySchema = new mongoose.Schema({
   // Field 1: Define a property for 'code' with a String data type and required true
   // Field 2: Define a property for 'name' with a String data type
   // Field 3: Define a property for 'exchangeRate' with a Number data type and required true
+  code: {
+    type: String,
+    required: true,
+  },
+  name: String,
+  exchangeRate: {
+    type: Number,
+    required: true,
+  },
 });
 
 const Currency = mongoose.model('Currency', currencySchema);
