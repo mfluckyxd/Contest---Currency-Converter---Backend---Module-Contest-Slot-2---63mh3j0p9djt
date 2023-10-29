2e15737689bddf74f529413e3efb714e9ebc483d
Kamal Bijaraniyan <52608133+mfluckyxd@users.noreply.github.com>
2023-10-29 18:46:59 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const currencyRoutes = require('../routes/currencyRoutes');
+const mongoose = require('mongoose');
 
 app.use(express.json());
 
